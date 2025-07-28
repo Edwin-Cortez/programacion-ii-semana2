640db7e463125dffccea615e28dd0e00124543f8
Edwin-Cortez <ecortezt@miumg.edu.gt>
2025-07-27 22:53:28 -0600

    Add files via upload

---
+++ Semana2_Edad.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Semana2_Edad {
+    public static void main(String[] args) {
+
+      //creamos un objeto scanner para leer la entrada desde el teclado
+        Scanner scanner = new Scanner(System.in);
+
+        //solicitamos el nombre del usuario
+        System.out.println("Ingrese su nombre: ");
+        String nombre = scanner.nextLine();   //utilizamos variable tipo string
+
+        //solicitamos el año de nacimiento
+        System.out.print("Ingrese su año de nacimiento:");
+        int añoNacimiento = scanner.nextInt();   // utilizamos variable tipo int
+
+        //calculamos la edad aproximda usando el año 2,025
+        int edad = 2025 - añoNacimiento;
+
+        //mostramos el resultado en la consola
+        System.out.print("Hola " + nombre + ", tu edad aproximada es:" + edad + " años.");
+
+        //cerrar el scanner para liberar recursos
+        scanner.close();
+    }
+}
